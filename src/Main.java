d4a87f3eeb6a652431cdd3bab53af7e9b74e85cc
yeejfe <yeejfe@hotmail.com>
2017-11-16 01:54:18 +0800

    Check encrypted html

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Main {
     private static String codePath = new File(".").getAbsolutePath();
@@ -56,14 +65,16 @@ public class Main {
         } else {
             System.out.println("Is APK tool in current directory?");
         }
+        System.out.println();
         System.out.println("Exited with status: " + status);
     }
 
     private static void checkForCordovaFiles(String folderName) {
         String currentDir = new File(".").getAbsolutePath();
         currentDir = currentDir.substring(0, currentDir.length() - 1);
+        String dirPath = currentDir + folderName + "\\assets\\www";
 
-        File file = new File(currentDir + folderName + "\\assets\\www\\cordova.js");
+        File file = new File(dirPath + "\\cordova.js");
 
         boolean isCordova = false;
         if (file.exists()) {
@@ -72,5 +83,56 @@ public class Main {
 
         System.out.println();
         System.out.println("Cordova Application: " + isCordova);
+        
+        if(isCordova){
+            checkWhetherEncrypted(dirPath);
+        }
+    }
+
+    private static void checkWhetherEncrypted(String dirPath) {
+        System.out.println("Searching in " + dirPath + " ..." );
+        ArrayList<String> fileList = (ArrayList<String>) getFileNames(new ArrayList<String>(), Paths.get(dirPath));
+        System.out.println("Number of Encrypted html files: " + fileList.size());
+        
+        System.out.println("====Encrypted HTML Files====");
+        
+        for(String fileName : fileList){
+            System.out.println(fileName);
+        }
+    }
+
+    private static List<String> getFileNames(List<String> fileNames, Path dir) {
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path path : stream) {
+                if(path.toFile().isDirectory()) {
+                    getFileNames(fileNames, path);
+                } else {
+                    String fileName = path.getFileName().toString();
+                    if(fileName.endsWith(".html") || fileName.endsWith(".htm")){                      
+                        if(isFileEncrypted(path)){
+                            fileNames.add(path.toAbsolutePath().toString());
+                        }     
+                    }
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return fileNames;
+    }
+    
+    private static boolean isFileEncrypted(Path path) {
+        Document initialDoc;
+        try {
+            initialDoc = Jsoup.parse(new File(path.toString()), "UTF-8", "");
+
+            Jsoup.connect("http://validator.w3.org/check")
+                    .data("fragment", initialDoc.html())
+                    .data("st", "1")
+                    .post();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 }
